2d7ac2cd87d5c13fa59dcd37de04a462e4634adb
slavikusq75 <sl_msm@ukr.net>
2015-11-14 19:02:30 +0200

    The first commit

---
+++ frontend/config/db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slava
+ * Date: 13.11.15
+ * Time: 22:26
+ */
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=frontend.dev;dbname=pawnshop',
+    'username' => 'user',
+    'password' => 'password',
+    'charset' => 'utf8',
+];
---
+++ frontend/views/layouts/basic.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slava
+ * Date: 14.11.15
+ * Time: 16:43
+ */
+/* $var $content string */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<p>Upper part of the site</p>
+
+<?php $content ?>
+<p>Lower part of the site</p>
+</body>
+</html>
