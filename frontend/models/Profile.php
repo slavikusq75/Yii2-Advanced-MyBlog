8199cc25f10867d596ce46e0ef612d6585a33490
slavikusq75 <sl_msm@ukr.net>
2016-01-18 14:22:23 +0200

    Profile model is updated with yyyGender and yyyUser methods

--- frontend/models/Profile.php
+++ frontend/models/Profile.php
@@ -94,4 +94,66 @@ class Profile extends ActiveRecord
     {
         return $this->hasOne(Gender::className(), ['id' => 'gender_id']);
     }
+
+    /**
+     * uses magic getGender on return statement
+     *
+     */
+    public function getGenderName()
+    {
+        return $this->gender->gender_name;
+    }
+
+    /**
+     * get list of genders for dropdown
+     */
+    public static function getGenderList()
+    {
+        $droptions = Gender::find()->asArray()->all();
+        return Arrayhelper::map($droptions, 'id', 'gender_name');
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @get Username
+     */
+    public function getUsername()
+    {
+        return $this->user->username;
+    }
+
+    /**
+     * @getUserId
+     */
+    public function getUserId()
+    {
+        return $this->user ? $this->user->id : 'none';
+    }
+
+    /**
+     * @getUserLink
+     */
+    public function getUserLink()
+    {
+        $url = Url::to(['user/view', 'id'=>$this->UserId]);
+        $options = [];
+        return Html::a($this->getUserName(), $url, $options);
+    }
+
+    /**
+     * @getProfileLink
+     */
+    public function getProfileIdLink()
+    {
+        $url = Url::to(['profile/update', 'id'=>$this->id]);
+        $options = [];
+        return Html::a($this->id, $url, $options);
+    }
 }
