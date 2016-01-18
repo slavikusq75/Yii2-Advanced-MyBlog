b1d1db829c4afffd7ba3761138fa5f74610e08cc
slavikusq75 <sl_msm@ukr.net>
2016-01-18 11:06:43 +0200

    status table is created

---
+++ console/migrations/m160118_090212_create_status_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160118_090212_create_status_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('status', [
+            'id' => $this->primaryKey(),
+            'status_name' => $this->string(45)->notNull(),
+            'status_value' => $this->integer(6)->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('status');
+    }
+}
